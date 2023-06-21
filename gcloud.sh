a9dd4fbf113a87d817950c3e1124bc3d95c60868
Anand Taralika <46954145+taralika@users.noreply.github.com>
2023-06-20 19:00:28 -0700

    Update gcloud.sh to Python 3.9

--- gcloud.sh
+++ gcloud.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy quote --runtime python38 --trigger-http --allow-unauthenticated
+gcloud functions deploy quote --runtime python39 --trigger-http --allow-unauthenticated
